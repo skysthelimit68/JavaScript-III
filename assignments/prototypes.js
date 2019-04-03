bb993545551b10359a5d174962d65b2010e7857e
Ming Liu <mliu@macpro.home>
2019-04-03 14:28:50 -0400

    initial setup of game

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -144,4 +144,24 @@ Humanoid.prototype.greet = function () {
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
 
+  function Villain(atts) {
+    Humanoid.call(this, atts);
+    this.attack = function(opponent) {
+      opponent.healthPoints = opponent.healthPoints - randomizer();
+    }
+  }
+  Villain.prototype = Object.create(Humanoid.prototype);
+
+  function Hero(atts) {
+    Humanoid.call(this, atts);
+    this.attack = function(opponent) {
+      opponent.healthPoints = opponent.healthPoints - randomizer();
+    }
+  }
+  Hero.prototype = Object.create(Humanoid.prototype);
+
+  function randomizer(){
+    return Math.floor(Math.random() * 10) + 1;
+  }
+
  
